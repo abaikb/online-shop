d2f6d8eabeb09f7b8953c07a5a59988485086429
abaikb <abaikb@icloud.com>
2023-08-22 17:00:18 +0600

    add header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
+import { FaHeart, FaShoppingCart, FaUser } from 'react-icons/fa';
+import styles from './Header.module.css';
+import logo from './image/icon/logo.png';
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="/path/to/logo.png" alt="Логотип магазина" />
+    <header className={styles.header}>
+      <div className={`${styles.logo} ${styles.img}`}>
+        <img src={logo} alt="Логотип магазина" />
       </div>
-      <nav className="navigation">
-        <ul className="nav-list">
+      <div className={styles.search}>
+        <input type="text" placeholder="Поиск" />
+        <button>Найти</button>
+      </div>
+      <nav className={styles.navigation}>
+        <ul className={styles['nav-list']}>
           <li><a href="/">Главная</a></li>
           <li><a href="/catalog">Каталог</a></li>
           <li><a href="/about">О нас</a></li>
           <li><a href="/contact">Контакты</a></li>
+          <li><a href="/favorites"><FaHeart /> </a></li>
+          <li><a href="/cart"><FaShoppingCart /> </a></li>
+          <li><a href="/profile"><FaUser /> </a></li>
         </ul>
       </nav>
-      <div className="search">
-        <input type="text" placeholder="Поиск" />
-        <button>Найти</button>
-      </div>
-      <div className="cart">
-        <a href="/cart">
-          <img src="/path/to/cart-icon.png" alt="Иконка корзины" />
-          <span>Корзина</span>
-        </a>
-      </div>
     </header>
   );
 };
