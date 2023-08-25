bafe8deb4f454b65144961e41cf27be35dd66462
Samy4-byte <samatbayizov@gmail.com>
2023-08-25 14:51:04 +0600

    fix header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage/HomePage';
 import CatalogPage from './pages/catalogPage/bag';
 import './App.css';
-<<<<<<< HEAD
-import Header from './components/header/header.jsx';
-=======
 import CartPage from './pages/CartPage';
->>>>>>> d2fe550f91915740b6edc31f3451bdff40430dd9
 
 function App() {
   return (
