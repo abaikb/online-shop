90b37e1e5c3ddda64c6e681c4cbc1a39b8e55fab
Jendukkie <abduldahanovajenya01@gmail.com>
2023-08-30 09:22:45 +0600

    addToFavorite

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './CartContext'; // Импортируйте CartProvider
+import { FavoriteProvider } from './FavoriteContext';
+
 import Layout from './components/Layout/Layout';
 import HomePage from './pages/HomePage/HomePage';
 import CatalogPage from './pages/catalogPage/bag';
 import BagDetailPage from './pages/DetailPage/detail.jsx'; // Импортируйте компонент BagDetailPage
 import CartPage from './pages/CartPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import './App.css';
+import Favorite from './pages/FavoritePage/fav.jsx'; 
+;
 
 function App() {
   return (
     <Router>
       <CartProvider>
+      <FavoriteProvider>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
             <Route path="/catalog" element={<CatalogPage />} />
             <Route path="/cart" element={<CartPage />} />
+            <Route path="/favorites" element={<Favorite />} /> 
             <Route path="/catalog/:id" element={<BagDetailPage />} /> Добавьте маршрут для детальной страницы
             {/* Добавьте другие маршруты здесь */}
           </Route>
         </Routes>
+        </FavoriteProvider>
       </CartProvider>
     </Router>
   );
--- src/pages/DetailPage/detail.jsx
+++ src/pages/DetailPage/detail.jsx
@@ -1,50 +1,68 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './BagDetailPage.module.css';
 import { FaShoppingCart, FaHeart, FaMapMarkerAlt } from 'react-icons/fa';
+import { CartContext } from '../../CartContext';
 
-function BagDetailPage({ addToCart }) {
-  const { id } = useParams();
-  const [bag, setBag] = useState(null);
 
-  useEffect(() => {
-    async function fetchBag() {
-      try {
-        const response = await fetch(`https://645366c8c18adbbdfe9c3b99.mockapi.io/shop/events/${id}`);
-        const data = await response.json();
-        setBag(data);
-      } catch (error) {
-        console.error('Error fetching bag:', error);
-      }
+function BagDetailPage() {
+    const { id } = useParams();
+    const [bag, setBag] = useState(null);
+    const { cartItems, setCartItems } = useContext(CartContext);
+
+
+    useEffect(() => {
+        async function fetchBag() {
+            try {
+                const response = await fetch(`https://645366c8c18adbbdfe9c3b99.mockapi.io/shop/events/${id}`);
+                const data = await response.json();
+                setBag(data);
+            } catch (error) {
+                console.error('Error fetching bag:', error);
+            }
+        }
+
+        fetchBag();
+    }, [id]);
+
+    if (!bag) {
+        return <div>Loading...</div>;
     }
 
-    fetchBag();
-  }, [id]);
+    const addToCart = (item) => {
+        const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
+    
+        if (existingItem) {
+          setCartItems((prevCartItems) =>
+            prevCartItems.map((cartItem) =>
+              cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+            )
+          );
+        } else {
+          setCartItems((prevCartItems) => [...prevCartItems, { ...item, quantity: 1 }]);
+        }
+      };
 
-  if (!bag) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className={styles.bagDetailPage}>
-      <img className={styles.bagImage} src={bag.image} alt={bag.name} />
-      <div className={styles.bagInfo}>
-        <h2 className={styles.bagName}>{bag.name}</h2>
-        <div className={styles.icons}>
-          <FaHeart className={styles.icon} />
-          <p className={styles.bagPrice}>Цена: {bag.price} сом</p>
-          <FaShoppingCart className={styles.icon} onClick={() => addToCart(bag)} />
+    return (
+        <div className={styles.bagDetailPage}>
+            <img className={styles.bagImage} src={bag.image} alt={bag.name} />
+            <div className={styles.bagInfo}>
+                    <h2 className={styles.bagName}>{bag.name}</h2>
+                    <div className={styles.icons}>
+                        <FaHeart className={styles.icon} />
+                        <p className={styles.bagPrice}>Цена: {bag.price} сом</p>
+                        <FaShoppingCart className={styles.icon} onClick={() => addToCart(bag)} />
+                    </div>
+                <p className={styles.bagDesc}>{bag.desc}</p>
+                <p className={styles.bagDesc}>{bag.info}</p>
+                <div className={styles.bagPlace}>
+                    <FaMapMarkerAlt className={styles.locationIcon} />
+                    <p>{bag.place}</p>
+                </div>
+                <p className={styles.bagDate}>Дата: {bag.date}</p>
+            </div>
         </div>
-        <p className={styles.bagDesc}>{bag.desc}</p>
-        <p className={styles.bagDesc}>{bag.info}</p>
-        <div className={styles.bagPlace}>
-          <FaMapMarkerAlt className={styles.locationIcon} />
-          <p>{bag.place}</p>
-        </div>
-        <p className={styles.bagDate}>Дата: {bag.date}</p>
-      </div >
-    </div >
-  );
+    );
 }
 
 export default BagDetailPage;
