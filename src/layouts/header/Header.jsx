9f60b10aa9c3d7cab9817c62efe630db7ca11af5
Messier1703 <112243409+Messier1703@users.noreply.github.com>
2023-10-01 21:30:54 +0600

    header adaptive

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { FaHeart, FaShoppingCart, FaUser, FaBars, FaTimes, FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
@@ -10,10 +10,10 @@ import ProfileIcon from '../../assets/images/icons/svg/profileIcon';
 import HeartIcon from '../../assets/images/icons/svg/heartIcon';
 import CartIcon from '../../assets/images/icons/svg/cartIcon';
 import { BurgerIcon } from '../../assets/images/icons/svg/burgerIcon';
-import BurgerMenu from '../../components/burgerMenu/BurgerMenu';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 748);
   const { cartItems } = useContext(CartContext);
   const { favorites } = useContext(FavoriteContext);
 
@@ -25,23 +25,71 @@ const Header = () => {
     setIsMenuOpen(false);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 748);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <header className={styles.header}>
-      {/* <BurgerMenu /> */}
+      {isMobile && isMenuOpen && (
+        <nav className={styles.burger_nav}>
+          <div className="container">
+            <div className={styles.close_button}>
+              <p onClick={closeMenu}>Закрыть</p>
+            </div>
+            <div className={styles.wrapper}>
+              <ul>
+                <Link>lorem ipsum</Link>
+                <Link>lorem isdfsdfpsum</Link>
+                <Link>lorem ipsum</Link>
+                <Link>lorem ipsum</Link>
+                <Link>lorem ipsum</Link>
+                <Link>lorem isfpsum</Link>
+                <Link>lorem ipsum</Link>
+                <Link>lorem ipsum</Link>
+              </ul>
+              <ul>
+                <Link>contact</Link>
+                <Link>contacfsdfsdt</Link>
+                <Link>contact</Link>
+                <Link>contact</Link>
+                <div>
+                  <Link>contact</Link>
+                  <Link>contact</Link>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      )}
       <div className="container">
         <nav className={styles.desktop_nav}>
-          <BurgerIcon />
-          <Link to="/" className={styles.header_logo}>
-            <img src={logo} alt="Логотип" />
-          </Link>
-          <ul>
-            <Link>lorem</Link>
-            <Link>lorem</Link>
-            <Link>lorem</Link>
-            <Link>lorem</Link>
-            <Link>lorem</Link>
-            <Link>lorem</Link>
-          </ul>
+          <div className={styles.header_nav}>
+            {isMobile && (
+              <button className={styles.burger_button} onClick={toggleMenu}>
+                <BurgerIcon />
+              </button>
+            )}
+            <Link to="/" className={styles.header_logo}>
+              <img src={logo} alt="Логотип" />
+            </Link>
+          </div>
+          {isMobile === false && (
+            <ul>
+              <Link>lorem</Link>
+              <Link>lorem</Link>
+              <Link>lorem</Link>
+              <Link>lorem</Link>
+              <Link>lorem</Link>
+              <Link>lorem</Link>
+            </ul>
+          )}
           <ul>
             <Link>icon</Link>
             <Link>icon</Link>
